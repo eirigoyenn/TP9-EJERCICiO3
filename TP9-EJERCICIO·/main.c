1732272e9351c3e2991301e579adc2ffc60ee228
enrique <enrique@enrique-VirtualBox>
2019-10-17 11:53:11 -0300

    pruebA

---
+++ TP9-EJERCICIO·/main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: enrique
+ *
+ * Created on 17 de octubre de 2019, 11:51
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("hola");
+    
+    return (EXIT_SUCCESS);
+}
+
