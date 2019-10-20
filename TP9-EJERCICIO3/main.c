db6d879ea27456b89240a276f9ddb87ffd2e3d02
enrique <enrique@enrique-VirtualBox>
2019-10-20 10:53:46 -0300

    Ejercicio3-Funcion  Modificada

--- TP9-EJERCICIO3/main.c
+++ TP9-EJERCICIO3/main.c
@@ -7,16 +7,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 int suma_digitos(int n1);
 /*
  * 
  */
 int main(void) {
     
-    int numero=123456;
-    int valor;
+    int numero=123456;   /*Numero elegido para probar el programa*/
+    int valor;           /*Variable para guardar el valor de la suma de los digitos*/
     
-    valor=suma_digitos(numero);
+    valor=suma_digitos(numero);          /*llamo a la funcion para obtener la suma de los digitos de "numero" */
     
     printf("La suma de los digitos del numero: %d \n",numero);
     printf("Es : %d \n", valor);
@@ -26,10 +27,10 @@ int main(void) {
 
 int suma_digitos(int n1){
     
-    if(n1<10){
+    if(n1<10){          /*Caso base: si el numero es menor a 10(de 1 cifra) ,devuelvo ese numero*/
         
         return n1;
     }
  
-    return ((n1 % 10) + suma_digitos(n1 / 10)); //caso recursivo
+    return ((n1 % 10) + suma_digitos(n1 / 10)); /*caso recursivo: devuelvo el modulo del numero divido 10 y llamo a la funcion con el numero dividido 10*/
 }
